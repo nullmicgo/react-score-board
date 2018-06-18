661f4fda38a77d5bec36358e0ecfe8ef9fe25f55
nullmicgo@gmail.com <nullmicgo@gmail.com>
2018-06-18 12:58:59 +0800

    Creating Actions and Action Creators

---
+++ src/actions/player.js
@@ -0,0 +1,26 @@
+import * as PlayerActionTypes from '../actiontypes/player';
+
+export const addPlayer = name => {
+    return {
+        type: PlayerActionTypes.ADD_PLAYER,
+        name
+    };
+};
+
+export const removePlayer = index => {
+    return {
+        type: PlayerActionTypes.REMOVE_PLAYER,
+        index
+    };
+};
+
+export const updatePlaterScore = (index, score) =>{
+    return {
+        type: PlayerActionTypes.UPDATE_PLAYER_SCORE,
+        index,
+        score
+    };
+};
+
+
+
